3a36dd7a552fa3bd5b81ff5174ad1386aa1a4e41
Bruno Vieira <125419291+BrunoVieiraSantana@users.noreply.github.com>
2023-12-20 21:16:26 -0300

    Delete javascript 19-12 - manhã directory

--- javascript 19-12 - manhã/ECMAScript-Modules-ESM/app.js
+++
@@ -1,7 +0,0 @@
-import { calculadora } from './menu.js';
-
-console.log(calculadora('soma', 5, 3));
-console.log(calculadora('subtracao', 7, 2));
-console.log(calculadora('multiplicacao', 4, 6));
-console.log(calculadora('divisao', 8, 2));
-console.log(calculadora('divisao', 8, 0));
