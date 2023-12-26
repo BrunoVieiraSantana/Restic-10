97113c20774043372e63b440077071496a3b8593
BrunoVieiraSantana <brunonewdev@gmail.com>
2023-12-26 16:55:50 -0300

    bee update 2

--- beecrowd/1019.js
+++ beecrowd/1019.js
@@ -2,3 +2,8 @@ let input = require('fs').readFileSync('input1019.txt', 'utf8');
 let lines = input.split(/\r?\n/);
 
 let n = parseInt(lines.shift());
+let h = Math.floor(n/3600)
+let m = Math.floor((n-(h*3600))/60)
+let s = Math.floor((n-(h*3600+m*60))/1)
+
+console.log(`${h}:${m}:${s}`)
