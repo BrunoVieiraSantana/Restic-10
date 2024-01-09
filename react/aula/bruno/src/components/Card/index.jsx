bd5155c2948c7944340de82aac977335bf1ddc7e
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-01-09 11:15:43 -0300

    react

--- react/aula/bruno/src/components/Card/index.jsx
+++ react/aula/bruno/src/components/Card/index.jsx
@@ -5,16 +5,32 @@ import { BsArrow90DegRight } from "react-icons/bs";
 
 export default function Card(){
     return(
-        <div >
+        <div className="box">
             <img className={estilo.logo} src="img/Ellipse-1.png" />
-            <p className={estilo.nome}>Istituto irede</p>
-            <p>12h <BiGlobe /></p>
+            <p className={estilo.nome}>Istituto IREDE</p>
+            <p className={estilo.hora}>12h <BiGlobe /></p>
             <img className={estilo.main} src="img/Rectangle-1.png" />
             <nav>
-                <p>Curtir <BiLike /> </p>
-                <p>Comentar<BiComment /> </p>
-                <p>Compartilhar <BsArrow90DegRight /> </p>
+                <p className={estilo.botao01}>Curtir <BiLike /> </p>
+                <p className={estilo.botao02}>Comentar<BiComment /> </p>
+                <p className={estilo.botao03}>Compartilhar <BsArrow90DegRight /> </p>
             </nav>
         </div>
     )
 }
+
+// import React from "react";
+// import "./style.css";
+
+// export const Box = () => {
+//   return (
+//     <div className="box">
+//       <img className="card" alt="Card" src="card.png" />
+//     </div>
+//   );
+// };
+
+// import React from "react";
+// import "./style.css";
+
+
--- react/aula/bruno/src/components/Header/index.jsx
+++ react/aula/bruno/src/components/Header/index.jsx
@@ -1,9 +1,9 @@
-import styles from "./style.module.css" 
+import forma from "./style.module.css";
 
-export default function Header(){
-    return(
-        <div className={styles.exemplo}>
-            Facebook
-        </div>
-    )
+export default function Header() {
+  return (
+      <div className={forma["topo"]}>
+        <div className={forma["texto"]}>Facebook</div>
+      </div>
+  );
 }
