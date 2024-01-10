b99d8edb9b81e9b733a5f38571df33ec32621089
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-01-09 22:44:10 -0300

    react

---
+++ react/aula/bruno-tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header></Header>
+        <Card></Card>
+      </div>      
+    </>
+  )
+}
+
+export default App
---
+++ react/aula/bruno-tailwind/src/components/Card/index.jsx
@@ -0,0 +1,29 @@
+import { BiLike, BiComment, BiGlobe } from "react-icons/bi";
+import { BsArrow90DegRight } from "react-icons/bs";
+{/* ml, mr, mt, mb */}
+export default function Card() {
+  return (
+    <div className="mx-auto items-center justify-center mt-[10px] ml-[370px]">
+      <div className="flex space-x-2">
+      <img className="w-12 h-12 rounded-full" src="img/Ellipse-1.png" alt="Profile" />
+            <div>
+                <div >
+                    <p className="text-black text-xs font-medium font-inter">Istituto IREDE</p>
+                </div>
+                <p className="flex space-x-2 text-neutral-400 text-xs font-medium font-inter">12h <BiGlobe /></p>
+            </div>
+        </div>
+      <img className="w-[278px] h-[188.32px]" src="img/Rectangle-1.png" alt="Post" />
+      <nav className="flex space-x-2">
+        <p className="text-neutral-400 text-xs font-medium font-inter">Curtir</p>
+        <p className="text-neutral-400 text-xs font-medium font-inter"><BiLike /></p>
+        <p className="text-neutral-400 text-xs font-medium font-inter">Comentar</p>
+        <p className="text-neutral-400 text-xs font-medium font-inter"><BiComment /></p>
+        <p className="text-neutral-400 text-xs font-medium font-inter">Compartilhar</p>
+        <p className="text-neutral-400 text-xs font-medium font-inter"><BsArrow90DegRight /></p>
+        </nav>
+        
+
+    </div>
+  );
+}
