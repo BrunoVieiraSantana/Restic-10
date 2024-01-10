152bd21b1829cadb49aeb463af3c4f22d2752fc1
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-01-10 06:10:31 -0300

    update

--- react/facebook-tailwind/src/components/Card/index.jsx
+++ react/facebook-tailwind/src/components/Card/index.jsx
@@ -3,8 +3,11 @@ import { BsArrow90DegRight } from "react-icons/bs";
 
 export default function Card() {
   return (
-    <div className="mx-auto items-center justify-center p-[10px] flex flex-col">
-      <div className="flex space-x-2 m-2">
+    <div className="mx-auto items-center justify-center p-[10px] flex flex-col relative">
+      <div className="w-[278px] h-[295px] absolute z-10 shadow-lg"></div>
+
+
+      <div className="flex space-x-2 m-2 z-20">
         <img className="w-12 h-12 rounded-full transform -translate-x-16" src="img/Ellipse-1.png" alt="Profile" />
         <div className="transform -translate-x-16 -translate-y-[-6px]">
           <div>
@@ -13,8 +16,8 @@ export default function Card() {
           <p className="flex space-x-2 text-neutral-400 text-xs font-medium font-inter">12h <BiGlobe /></p>
         </div>
       </div>
-      <img className="w-[278px] h-[188.32px]" src="img/Rectangle-1.png" alt="Post" />
-      <nav className="flex space-x-[10px] p-4">
+      <img className="w-[278px] h-[188.32px] z-20" src="img/Rectangle-1.png" alt="Post" />
+      <nav className="flex space-x-[10px] p-4 z-20">
         <p className="text-neutral-400 text-xs font-medium font-inter">Curtir</p>
         <p className="text-neutral-400 text-xs font-medium font-inter"><BiLike /></p>
         <p className="text-neutral-400 text-xs font-medium font-inter">Comentar</p>
@@ -22,7 +25,6 @@ export default function Card() {
         <p className="text-neutral-400 text-xs font-medium font-inter">Compartilhar</p>
         <p className="text-neutral-400 text-xs font-medium font-inter"><BsArrow90DegRight /></p>
       </nav>
-      {/* <div className="w-[288px] h-[230px] border-1 border-solid border-gray-300 shadow-md bg-pink-500"></div> */}
     </div>
   );
 }
