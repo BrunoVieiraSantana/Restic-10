348d12faba6b3099e85cd026aa88fdc37dd46449
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-01-17 16:31:14 -0300

    react update-5

---
+++ react/Aula03-tarde-main/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import Header from "./components/Header"
+import Card from "./components/Card"
+import Footer from "./components/Footer"
+import imagem from "./assets/sanduba.png"
+import kebab from "./assets/kebab.png"
+
+function App() {
+  
+  const [total, setTotal] = useState (0)
+  console.log(setTotal)
+  localStorage.setItem("quente",(setTotal))
+
+  const products = [{id: "1",titulo: "sanduba", descricao: "Experimente nosso hambúrguer premium: carne suculenta, queijo derretido, vegetais frescos (alface, tomate, cebola roxa) e molho especial. Uma combinação perfeita de sabores e texturas em cada mordida.", figura: imagem}, {id: "2",titulo: "kebab", descricao: "Experimente nosso hambúrguer premium: carne suculenta, queijo derretido, vegetais frescos (alface, tomate, cebola roxa) e molho especial. Uma combinação perfeita de sabores e texturas em cada mordida.", figura: kebab}]
+
+  return (
+    <>
+     <Header/>
+    <div className="flex flex-col gap-5 items-center">
+    {products.map((product)=> (<Card  product={product}  setTotal={setTotal} key={product.id}/>))}    
+    </div>
+     {/* <Card cachorro={itens} setItens2={setItens}/>
+     <Card cachorro={itens2} setItens2={setItens2}/> */}
+     {total>0?<Footer quente={total}/>:null}
+     
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ react/Aula03-tarde-main/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <>
+        <header className="mb-5 flex justify-center h-20 items-center bg-primary">
+            <h1 className="text-white">IredeFOOD</h1>
+        </header>
+        </>
+    )
+}
