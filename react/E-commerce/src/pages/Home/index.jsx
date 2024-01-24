7a724377fca29d609a64e9420336e4bb6dcfe9a2
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-01-24 09:55:07 -0300

    E-commerce inicial-3

---
+++ react/E-commerce/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useContext } from "react"
+
+export default function Home() {
+    return (
+        <main>
+            <section className="flex flex-wrap  justify-center">
+                Testando
+            </section>
+        </main>
+    )
+}
