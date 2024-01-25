ca1bbff4a854e8408c1ee23f1b54d66318e76356
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-01-25 11:47:47 -0300

    E-commerce inicial-9

--- react/E-commerce/src/components/Header/index.jsx
+++ react/E-commerce/src/components/Header/index.jsx
@@ -1,7 +1,14 @@
 export default function Header() {
     return (
-        <div>
-          <div>Header</div>
-        </div>
+      <>
+        <img className="" src="./src/assets/menu.svg"></img>
+        <img className="" src="./src/assets/logo.png"></img>
+        <img className="" src="./src/assets/cart.svg"></img>
+        <br/>
+        <input className="rounded-md" placeholder="Buscar"/>
+      </>
     );
   }
+
+
+  
--- react/E-commerce/tailwind.config.js
+++ react/E-commerce/tailwind.config.js
@@ -5,8 +5,23 @@ export default {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-700": "var(--blue-700)",
+        "blue-900": "var(--blue-900)",
+        "from-lime-50-to-zinc-50": "var(--from-lime-50-to-zinc-50)",
+        "green-700": "var(--green-700)",
+        "orange-500": "var(--orange-500)",
+        "red-600": "var(--red-600)",
+        "stone-500": "var(--stone-500)",
+        "stone-900": "var(--stone-900)",
+        "variable-collection-slate-100": "var(--variable-collection-slate-100)",
+        "variable-collection-white": "var(--variable-collection-white)",
+        "variable-collection-zinc-50": "var(--variable-collection-zinc-50)",
+      },
+    },
   },
   plugins: [],
 }
+
 
