5334a50f99467aad0212b02dec1e7c79499f0619
BrunoVieiraSantana <brunonewdev@gmail.com>
2024-01-26 11:17:36 -0300

    E-commerce inicial-11

--- react/E-commerce/src/components/Header/index.jsx
+++ react/E-commerce/src/components/Header/index.jsx
@@ -6,9 +6,29 @@ export default function Header() {
         <img className="w-[30.0%]" src="./src/assets/logo.png"></img>
         <img className="w-[5.0%]" src="./src/assets/cart.svg"></img>
       </div>
-      <div className="flex justify-center items-center pb-4">
-        <input className="w-[94.0%]" placeholder="Buscar"/>
-      </div>
+      <form className="flex justify-center items-center pb-4">
+            <div className="relative">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                </svg>
+                <input
+                    type="text"
+                    placeholder="Buscar"
+                    className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+                />
+            </div>
+        </form>
     </div>
     );
   }
